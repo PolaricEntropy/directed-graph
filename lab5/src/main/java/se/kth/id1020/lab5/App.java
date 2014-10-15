ca1066c4ef303030a73e1b83cb15391283e7affa
Björn Ehrby <behrby@kth.se>
2014-10-15 15:22:32 +0200

    Initial commit.

---
+++ lab5/src/main/java/se/kth/id1020/lab5/App.java
@@ -0,0 +1,13 @@
+package se.kth.id1020.lab5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
